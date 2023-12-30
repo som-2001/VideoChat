6cc56e61e3b3e3dbd570ad3df8ef28ed9c8d626d
som-2001 <100964164+som-2001@users.noreply.github.com>
2023-12-30 18:58:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const port =3001;
 
 app.use(
     cors({
-        origin:['https://video-chat-tt41.vercel.app/'],
+        origin:['*'],
         methods:["GET","POST"],
         Credentials:true,
     })
